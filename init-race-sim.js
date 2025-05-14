13cae6f15d98dba6adb09701ac65d6be26c8f248
gorolykmaxim <goroliuk.maksym@gmail.com>
2025-05-14 20:21:41 +0300

    Gracefully handle case if some of the games are not installed

--- init-race-sim.js
+++ init-race-sim.js
@@ -18,112 +18,117 @@ async function writeConfigFile(path, obj) {
 }
 
 async function initAcc(hourOfDay, weatherGradient) {
-    const configRootPath = path.join(os.homedir(), "Documents", "Assetto Corsa Competizione", "Config");
-    const menuSettingsPath = path.join(configRootPath, "menuSettings.json");
-    const menuSettings = await readConfigFile(menuSettingsPath);
-    menuSettings.weatherType = "Custom";
-    const customRace = menuSettings.seasonRaceEventData?.Free?.raceEventData?.CustomRace;
-    if (customRace) {
-        console.log(`ACC time of day: ${hourOfDay}:00`);
-        customRace.p1_TimeOfDay = hourOfDay;
-        customRace.q_TimeOfDay = hourOfDay;
-        customRace.r1_TimeOfDay = hourOfDay;
+    try {
+        const configRootPath = path.join(os.homedir(), "Documents", "Assetto Corsa Competizione", "Config");
+        const menuSettingsPath = path.join(configRootPath, "menuSettings.json");
+        const menuSettings = await readConfigFile(menuSettingsPath);
+        menuSettings.weatherType = "Custom";
+        const customRace = menuSettings.seasonRaceEventData?.Free?.raceEventData?.CustomRace;
+        if (customRace) {
+            console.log(`ACC time of day: ${hourOfDay}:00`);
+            customRace.p1_TimeOfDay = hourOfDay;
+            customRace.q_TimeOfDay = hourOfDay;
+            customRace.r1_TimeOfDay = hourOfDay;
+        }
+        await writeConfigFile(menuSettingsPath, menuSettings);
+        let weather;
+        let track = {
+            "idealLineGrip": 0.9800000190734864,
+            "outsideLineGrip": 0.0,
+            "wetLevel": 0.0,
+            "puddlesLevel": 0.0,
+            "wetDryLineLevel": 0.0,
+            "marblesLevel": 0.0
+        };
+        if (weatherGradient < 0.16) {
+            console.log("ACC weather: clear");
+            weather = {
+                "ambientTemperature": 27.0,
+                "roadTemperature": 38.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 0.25,
+                "rainLevel": 0.0
+            };
+        } else if (weatherGradient < 0.32) {
+            console.log("ACC weather: light cloudy");
+            weather = {
+                "ambientTemperature": 27.0,
+                "roadTemperature": 38.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 0.40,
+                "rainLevel": 0.0
+            };
+        } else if (weatherGradient < 0.5) {
+            console.log("ACC weather: cloudy");
+            weather = {
+                "ambientTemperature": 22.0,
+                "roadTemperature": 27.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 0.550000011920929,
+                "rainLevel": 0.0
+            };
+        } else if (weatherGradient < 0.625) {
+            console.log("ACC weather: light rain");
+            weather = {
+                "ambientTemperature": 21.0,
+                "roadTemperature": 26.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 0.6200000047683716,
+                "rainLevel": 0.119999997317791
+            };
+            track.wetLevel = 0.10999999940395357;
+            track.puddlesLevel = 0.20000000298023225;
+            track.wetDryLineLevel = 0.10999999940395357;
+        } else if (weatherGradient < 0.75) {
+            console.log("ACC weather: medium rain");
+            weather = {
+                "ambientTemperature": 20.0,
+                "roadTemperature": 19.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 0.699999988079071,
+                "rainLevel": 0.27000001072883608
+            };
+            track.wetLevel = 0.20000000298023225;
+            track.puddlesLevel = 0.30000001192092898;
+            track.wetDryLineLevel = 0.20000000298023225;
+        } else if (weatherGradient < 0.875) {
+            console.log("ACC weather: heavy rain");
+            weather = {
+                "ambientTemperature": 14.0,
+                "roadTemperature": 13.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 0.8799999952316284,
+                "rainLevel": 0.6200000047683716
+            };
+            track.wetLevel = 0.4000000059604645;
+            track.puddlesLevel = 0.4000000059604645;
+            track.wetDryLineLevel = 0.4000000059604645;
+        } else {
+            console.log("ACC weather: storm");
+            weather = {
+                "ambientTemperature": 11.0,
+                "roadTemperature": 10.0,
+                "windSpeed": 0.0,
+                "windDirection": 0.0,
+                "cloudLevel": 1.0,
+                "rainLevel": 0.8999999761581421
+            };
+            track.wetLevel = 0.699999988079071;
+            track.puddlesLevel = 0.699999988079071;
+            track.wetDryLineLevel = 0.699999988079071;
+        }
+        await writeConfigFile(path.join(configRootPath, "weatherStatus.json"), weather);
+        await writeConfigFile(path.join(configRootPath, "trackStatus.json"), track);
+    } catch (e) {
+        console.log("Failed to initialize ACC");
+        console.log(e);
     }
-    await writeConfigFile(menuSettingsPath, menuSettings);
-    let weather;
-    let track = {
-        "idealLineGrip": 0.9800000190734864,
-        "outsideLineGrip": 0.0,
-        "wetLevel": 0.0,
-        "puddlesLevel": 0.0,
-        "wetDryLineLevel": 0.0,
-        "marblesLevel": 0.0
-    };
-    if (weatherGradient < 0.16) {
-        console.log("ACC weather: clear");
-        weather = {
-            "ambientTemperature": 27.0,
-            "roadTemperature": 38.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 0.25,
-            "rainLevel": 0.0
-        };
-    } else if (weatherGradient < 0.32) {
-        console.log("ACC weather: light cloudy");
-        weather = {
-            "ambientTemperature": 27.0,
-            "roadTemperature": 38.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 0.40,
-            "rainLevel": 0.0
-        };
-    } else if (weatherGradient < 0.5) {
-        console.log("ACC weather: cloudy");
-        weather = {
-            "ambientTemperature": 22.0,
-            "roadTemperature": 27.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 0.550000011920929,
-            "rainLevel": 0.0
-        };
-    } else if (weatherGradient < 0.625) {
-        console.log("ACC weather: light rain");
-        weather = {
-            "ambientTemperature": 21.0,
-            "roadTemperature": 26.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 0.6200000047683716,
-            "rainLevel": 0.119999997317791
-        };
-        track.wetLevel = 0.10999999940395357;
-        track.puddlesLevel = 0.20000000298023225;
-        track.wetDryLineLevel = 0.10999999940395357;
-    } else if (weatherGradient < 0.75) {
-        console.log("ACC weather: medium rain");
-        weather = {
-            "ambientTemperature": 20.0,
-            "roadTemperature": 19.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 0.699999988079071,
-            "rainLevel": 0.27000001072883608
-        };
-        track.wetLevel = 0.20000000298023225;
-        track.puddlesLevel = 0.30000001192092898;
-        track.wetDryLineLevel = 0.20000000298023225;
-    } else if (weatherGradient < 0.875) {
-        console.log("ACC weather: heavy rain");
-        weather = {
-            "ambientTemperature": 14.0,
-            "roadTemperature": 13.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 0.8799999952316284,
-            "rainLevel": 0.6200000047683716
-        };
-        track.wetLevel = 0.4000000059604645;
-        track.puddlesLevel = 0.4000000059604645;
-        track.wetDryLineLevel = 0.4000000059604645;
-    } else {
-        console.log("ACC weather: storm");
-        weather = {
-            "ambientTemperature": 11.0,
-            "roadTemperature": 10.0,
-            "windSpeed": 0.0,
-            "windDirection": 0.0,
-            "cloudLevel": 1.0,
-            "rainLevel": 0.8999999761581421
-        };
-        track.wetLevel = 0.699999988079071;
-        track.puddlesLevel = 0.699999988079071;
-        track.wetDryLineLevel = 0.699999988079071;
-    }
-    await writeConfigFile(path.join(configRootPath, "weatherStatus.json"), weather);
-    await writeConfigFile(path.join(configRootPath, "trackStatus.json"), track);
 }
 
 function calculateLmuSky(weatherGradient) {
@@ -193,57 +198,62 @@ function createLmuSessionWeather(sky, weatherGradient, realRoadPreset) {
 }
 
 async function initLmu(hourOfDay, weatherGradient) {
-    const configRootPath = path.join(
-        process.env["ProgramFiles(x86)"],
-        "Steam",
-        "steamapps",
-        "common",
-        "Le Mans Ultimate",
-        "UserData",
-        "player"
-    );
-    const settingsPath = path.join(configRootPath, "Settings.json");
-    const settings = await readConfigFile(settingsPath);
-    const raceConditions = settings['Race Conditions'];
-    if (raceConditions) {
-        let minutesSinceMidnight;
-        if (Math.random() < 0.5) {
-            minutesSinceMidnight = hourOfDay * 60 + 30;
-            console.log(`LMU time of day: ${hourOfDay}:30`);
-        } else {
-            minutesSinceMidnight = hourOfDay * 60;
-            console.log(`LMU time of day: ${hourOfDay}:00`);
+    try {
+        const configRootPath = path.join(
+            process.env["ProgramFiles(x86)"],
+            "Steam",
+            "steamapps",
+            "common",
+            "Le Mans Ultimate",
+            "UserData",
+            "player"
+        );
+        const settingsPath = path.join(configRootPath, "Settings.json");
+        const settings = await readConfigFile(settingsPath);
+        const raceConditions = settings['Race Conditions'];
+        if (raceConditions) {
+            let minutesSinceMidnight;
+            if (Math.random() < 0.5) {
+                minutesSinceMidnight = hourOfDay * 60 + 30;
+                console.log(`LMU time of day: ${hourOfDay}:30`);
+            } else {
+                minutesSinceMidnight = hourOfDay * 60;
+                console.log(`LMU time of day: ${hourOfDay}:00`);
+            }
+            raceConditions['Practice1StartingTime'] = minutesSinceMidnight;
+            raceConditions['QualifyingStartingTime'] = minutesSinceMidnight;
+            raceConditions['RaceStartingTime'] = minutesSinceMidnight;
         }
-        raceConditions['Practice1StartingTime'] = minutesSinceMidnight;
-        raceConditions['QualifyingStartingTime'] = minutesSinceMidnight;
-        raceConditions['RaceStartingTime'] = minutesSinceMidnight;
+        await writeConfigFile(settingsPath, settings);
+        const trackWeather = {};
+        const sky = calculateLmuSky(weatherGradient);
+        trackWeather["Practice"] = createLmuSessionWeather(sky, weatherGradient, "LIGHT");
+        trackWeather["Qualifying"] = createLmuSessionWeather(sky, weatherGradient, "MEDIUM");
+        trackWeather["Race"] = createLmuSessionWeather(sky, weatherGradient, "HEAVY");
+        const weatherRootPath = path.join(configRootPath, "Settings");
+        await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWEC_ENDCEs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWEC_OUTERs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWEC_PADDOCKs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Circuit Of The Americas", "COTAWEC_NATIONALs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Circuit Of The Americas", "COTAWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Fuji", "FUJIWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Fuji_Cl", "FUJIWEC_CLs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Imola", "IMOLAWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Interlagos", "INTERLAGOSWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Lemans", "LEMANSWEC_MULSANNEs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Lemans", "LEMANSWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Monza", "MONZAWEC_GRANDEs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Monza", "MONZAWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Portimao", "PORTIMAOWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Sebring", "SEBRINGWEC_SCHOOLs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Sebring", "SEBRINGWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Spa", "SPAWECs.wet"), trackWeather);
+        await writeConfigFile(path.join(weatherRootPath, "Spa_Endce", "SPAWEC_ENDCEs.wet"), trackWeather);
+    } catch (e) {
+        console.log("Failed to initialize LMU");
+        console.log(e);
     }
-    await writeConfigFile(settingsPath, settings);
-    const trackWeather = {};
-    const sky = calculateLmuSky(weatherGradient);
-    trackWeather["Practice"] = createLmuSessionWeather(sky, weatherGradient, "LIGHT");
-    trackWeather["Qualifying"] = createLmuSessionWeather(sky, weatherGradient, "MEDIUM");
-    trackWeather["Race"] = createLmuSessionWeather(sky, weatherGradient, "HEAVY");
-    const weatherRootPath = path.join(configRootPath, "Settings");
-    await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWEC_ENDCEs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWEC_OUTERs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWEC_PADDOCKs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Bahrain", "BAHRAINWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Circuit Of The Americas", "COTAWEC_NATIONALs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Circuit Of The Americas", "COTAWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Fuji", "FUJIWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Fuji_Cl", "FUJIWEC_CLs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Imola", "IMOLAWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Interlagos", "INTERLAGOSWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Lemans", "LEMANSWEC_MULSANNEs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Lemans", "LEMANSWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Monza", "MONZAWEC_GRANDEs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Monza", "MONZAWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Portimao", "PORTIMAOWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Sebring", "SEBRINGWEC_SCHOOLs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Sebring", "SEBRINGWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Spa", "SPAWECs.wet"), trackWeather);
-    await writeConfigFile(path.join(weatherRootPath, "Spa_Endce", "SPAWEC_ENDCEs.wet"), trackWeather);
 }
 
 function pickHourOfDay() {
