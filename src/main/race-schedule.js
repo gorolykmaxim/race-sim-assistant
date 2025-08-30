be29e7999f55d9dcd0a96f192e9d8890fc329302
gorolykmaxim <goroliuk.maksym@gmail.com>
2025-08-30 13:16:38 +0300

    Bring back MP race fetching

---
+++ src/main/race-schedule.js
@@ -0,0 +1,89 @@
+import {parse} from "node-html-parser";
+
+async function fetchLMURF2RaceSchedules() {
+    try {
+        const result = [];
+        const url = "https://www.racecontrol.gg";
+        const res = await fetch(url);
+        const raw = await res.text();
+        const html = parse(raw);
+        for (const section of html.querySelectorAll("body div.container section")) {
+            if ("Upcoming Daily Races" !== section.querySelector("div h3")?.innerHTML) {
+                continue;
+            }
+            const gameSchedule = {
+                logoImageUrl: url + section.querySelector("div div img").getAttribute("src"),
+                races: [],
+            };
+            for (const card of section.querySelectorAll("div.scheduled-race-card")) {
+                const raceHeaders = card.querySelectorAll("div.race-info div.race_header span");
+                gameSchedule.races.push({
+                    name: card.querySelector("div.race-info h4").innerHTML.trim(),
+                    logoImageUrl: card.querySelector("div a img").getAttribute("src"),
+                    trackName: raceHeaders[raceHeaders.length - 1].innerHTML.trim()
+                });
+            }
+            result.push(gameSchedule);
+        }
+        return result;
+    } catch (e) {
+        console.log("Failed to fetch LMU/RF2 schedule");
+        console.log(e);
+        return [];
+    }
+}
+
+async function fetchLFMRaceSchedule() {
+    try {
+        const lfmUrl = "https://lowfuelmotorsport.com";
+        const res = await fetch("https://api3.lowfuelmotorsport.com/api/seasons/getMinifiedSeasonBySim");
+        const json = await res.json();
+        const accSeries = Object.values(json["series"]).find(v => v["simulation"] === "Assetto Corsa Competizione");
+        const races = [];
+        const GT3_CLASS = 0;
+        const GT4_CLASS = 1;
+        const tracks = [new Set(), new Set()];
+        for (const series of accSeries["series"]) {
+            if (series["event_style"] !== "daily") {
+                continue;
+            }
+            const name = series["series_name"];
+            const nameUpper = name.toUpperCase();
+            let seriesClass;
+            if (nameUpper.indexOf("GT3") !== -1) {
+                seriesClass = GT3_CLASS;
+            } else if (nameUpper.indexOf("GT4") !== -1) {
+                seriesClass = GT4_CLASS;
+            } else {
+                continue;
+            }
+            const trackName = series["active_track"]["track_name"];
+            const classTracks = tracks[seriesClass];
+            if (classTracks.has(trackName)) {
+                continue;
+            }
+            races.push({
+                name: name,
+                logoImageUrl: lfmUrl + "/assets/img/seriesbg/" + series["background"],
+                trackName,
+            });
+            classTracks.add(trackName);
+        }
+        return [{
+            logoImageUrl: lfmUrl + accSeries["icon_big"],
+            races,
+        }];
+    } catch (e) {
+        console.log("Failed to fetch LFM schedule");
+        console.log(e);
+        return [];
+    }
+}
+
+export async function fetchRaceSchedule() {
+    const results = await Promise.all([
+        fetchLMURF2RaceSchedules(),
+        fetchLFMRaceSchedule()
+    ]);
+    return results.flat();
+}
--- src/renderer/src/components/race-schedule/RaceSchedule.jsx
+++ src/renderer/src/components/race-schedule/RaceSchedule.jsx
@@ -12,6 +12,38 @@ import AMS2IMSALogo from "../../assets/race-sim-logos/ams2-imsa.png";
 import AMS2IndyCarLogo from "../../assets/race-sim-logos/ams2-indycar.png";
 import seedrandom from "seedrandom";
 
+function LoadingPlaceholder() {
+    return <div className={"d-flex justify-content-center mt-2 mb-2"}>
+        <div className={"spinner-border"} role={"status"}>
+            <span className={"visually-hidden"}>Loading...</span>
+        </div>
+    </div>;
+}
+
+function Race({title, subtitle, thumbnail, logo}) {
+    return <div className={"col"}>
+        <div className={"card text-bg-dark"}>
+            <img src={thumbnail} className={"card-img race-image opacity-50"} alt={title}/>
+            <div className={"card-img-overlay d-flex flex-column justify-content-between"}>
+                <div>
+                    <h5 className={"card-title"}>{title}</h5>
+                    <p className={"card-text"}>{subtitle}</p>
+                </div>
+                <img src={logo} className={"race-sim-logo align-self-end"}/>
+            </div>
+        </div>
+    </div>;
+}
+
+function Schedule({title, className, children}) {
+    return <div className={className}>
+        <h3 className={"mt-4 mb-2"}>{title}</h3>
+        <div className={"row row-cols-4 g-4"}>
+            {children}
+        </div>
+    </div>;
+}
+
 function pickRandomTrack(tracks) {
     tracks = tracks.slice();
     const msInDay = 1000 * 60 * 60 * 24;
@@ -31,31 +63,76 @@ function pickRandomTrack(tracks) {
     return {name: track.name, thumbnail: track.thumbnail, layout};
 }
 
-function Race({raceSimLogo, tracks}) {
+function SingleplayerRace({raceSimLogo, tracks}) {
     const [track, setTrack] = useState({});
     useEffect(() => setTrack(pickRandomTrack(tracks)), []);
-    return (
-        <div className={"col"}>
-            <div className={"card text-bg-dark"}>
-                <img src={track.thumbnail} className={"card-img race-image opacity-50"} alt={track.name}/>
-                <div className={"card-img-overlay d-flex flex-column justify-content-between"}>
-                    <div>
-                        <h5 className={"card-title"}>{track.name}</h5>
-                        <p className={"card-text"}>{track.layout}</p>
-                    </div>
-                    <img src={raceSimLogo} className={"race-sim-logo align-self-end"}/>
-                </div>
-            </div>
-        </div>
-    );
+    return <Race title={track.name} subtitle={track.layout} thumbnail={track.thumbnail} logo={raceSimLogo}/>;
+}
+
+function SingleplayerRaceSchedule({className}) {
+    return <Schedule title={"Singleplayer Races"} className={className}>
+        <SingleplayerRace raceSimLogo={LMULogo} tracks={TracksLMU}/>
+        <SingleplayerRace raceSimLogo={ACCLogo} tracks={TracksACC}/>
+        <SingleplayerRace raceSimLogo={AMS2IMSALogo} tracks={TracksAMS2IMSA}/>
+        <SingleplayerRace raceSimLogo={AMS2IndyCarLogo} tracks={TracksAMS2IndyCar}/>
+        <SingleplayerRace raceSimLogo={RF2Logo} tracks={TracksRF2}/>
+    </Schedule>;
+}
+
+function MultiplayerRaceSchedule() {
+    const [schedules, setSchedules] = useState(null);
+
+    async function init() {
+        const CACHE_TTL_MS = 2 * 60 * 60 * 1000;
+        let raceSchedule = localStorage.getItem("raceSchedule");
+        if (raceSchedule) {
+            raceSchedule = JSON.parse(raceSchedule);
+            if (Date.now() - raceSchedule.timestamp < CACHE_TTL_MS) {
+                setSchedules(raceSchedule.schedules);
+                return;
+            }
+        }
+        raceSchedule = {
+            schedules: await api.fetchRaceSchedule(),
+            timestamp: Date.now(),
+        };
+        localStorage.setItem("raceSchedule", JSON.stringify(raceSchedule));
+        setSchedules(raceSchedule.schedules);
+    }
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    if (schedules == null) {
+        return <LoadingPlaceholder/>;
+    } else {
+        let cards = [];
+        for (const schedule of schedules || []) {
+            const {logoImageUrl, races} = schedule;
+            races.sort((r1, r2) => {
+                if (r1.name < r2.name) {
+                    return -1;
+                } else if (r1.name === r2.name) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            });
+            for (const race of races) {
+                cards.push(
+                    <Race key={race.name} title={race.trackName} subtitle={race.name} thumbnail={race.logoImageUrl}
+                          logo={logoImageUrl}/>
+                );
+            }
+        }
+        return <Schedule title={"Multiplayer Races"}>{cards}</Schedule>;
+    }
 }
 
 export default function RaceSchedule({className}) {
-    return <div className={`row row-cols-4 g-4 ${className || ""}`}>
-        <Race raceSimLogo={LMULogo} tracks={TracksLMU}/>
-        <Race raceSimLogo={ACCLogo} tracks={TracksACC}/>
-        <Race raceSimLogo={AMS2IMSALogo} tracks={TracksAMS2IMSA}/>
-        <Race raceSimLogo={AMS2IndyCarLogo} tracks={TracksAMS2IndyCar}/>
-        <Race raceSimLogo={RF2Logo} tracks={TracksRF2}/>
-    </div>;
+    return <>
+        <SingleplayerRaceSchedule className={className}/>
+        <MultiplayerRaceSchedule/>
+    </>;
 }
